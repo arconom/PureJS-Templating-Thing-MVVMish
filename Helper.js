8264ae09814214d2b1a2c29f12f6e44b2bd0e0f9
arconom <arconom@hotmail.com>
2017-10-05 07:47:23 -0400

    Update Helper.js

--- Helper.js
+++ Helper.js
@@ -862,6 +862,31 @@ var Helper =
                     data.create(me, function () { });
                 });
             }
+        },
+        
+        getParameterByName: function(name, url) 
+        {
+            var regex, results;
+            
+            if (!url) 
+            {
+                url = window.location.href;
+            }
+            
+            name = name.replace(/[\[\]]/g, "\\$&");
+            regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+            results = regex.exec(url);
+            
+            if (!results) 
+            {
+                 return null;
+            }
+            if (!results[2]) 
+            {
+                return '';
+            } 
+            
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
         }
     }
 };
